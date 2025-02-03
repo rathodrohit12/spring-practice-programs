ab814d218b458b75f72c609ba29277223b763591
Rohit Rathod <rohit8064r@gmail.com>
2025-02-03 09:36:05 +0530

    programs added

--- Spring Security/SpringSecurityDemo3/src/main/java/com/rohit/springsecuritydemo3/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.rohit.springsecuritydemo3.service;
-
-import com.rohit.springsecuritydemo3.dto.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public interface UserService {
-    public User registerUser(User user);
-}
